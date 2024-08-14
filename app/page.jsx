40e362a90e7558729a873d117fee2bc23bd2cbc7
Felipe Wallace <felipewallacepc@gmail.com>
2024-08-13 22:23:54 -0300

    Home page

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import Feed from "@components/Feed"
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Restaurante Universitário da Unifei
+           <br className="max-md:hidden" />
+           <span className="orange_gradient text-center">Cardapio do dia  {/* data */}</span>
+        </h1>
+        <p className="desc text-center">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Asperiores, vel quis nemo esse ex officiis sint quidem. 
+            Nihil impedit ea itaque non maiores a distinctio magnam reprehenderit facilis accusantium. Aut?
+        </p>
+
+        {/* Feed - Cardapio */}
+        <Feed />
+    </section>
+  )
+}
+
+export default Home
---
+++ components/Nav.jsx
@@ -0,0 +1,61 @@
+'use client'
+
+import Link from "next/link"
+import Image from "next/image"
+import { useState, useEffect } from "react"
+
+// import { signIn, signOut, useSession, getProviders } from "next-auth/react"
+
+const Nav = () => {
+  const isUserLoggedIn = true
+  const isUserAdmin = true
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Restaurante Universitário</p>
+      </Link>
+
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/avaliar" className="black_btn">
+              Avaliar
+            </Link>
+          </div> 
+        ): (
+          <>
+
+          </>
+        )}
+      </div>
+
+      <div className="sm:flex hidden">
+        {isUserAdmin ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/criar-cardapio" className="black_btn">
+              Criar Cardapio
+            </Link>
+          </div> 
+        ): (
+          <>
+
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+
+    </nav>
+  )
+}
+
+export default Nav

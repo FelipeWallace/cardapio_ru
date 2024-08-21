8010df57e4a81b965d6e05d0b5a7a0ebbb4aeb50
Felipe Wallace <97401368+FelipeWallace@users.noreply.github.com>
2024-08-20 21:10:33 -0300

    Update page.jsx

---
+++ app/avisos/page.jsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
+const Avisos = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Avisos
